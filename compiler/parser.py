8bff685eceaf7650bcfedc4dacad773128b9ec81
Salah <salahalsaif05@gmail.com>
2025-01-27 23:37:34 +0100

    Revert "fix"
    This reverts commit ac5e10725423acd3c383740d62f9c2308632f284.

--- compiler/parser.py
+++ compiler/parser.py
@@ -33,6 +33,10 @@ class Parser:
                 if "condition" not in token:
                     raise SyntaxError(f"Missing 'condition' or 'block' in {token_type} statement.")
 
+            elif token_type == TokenType.PENGUIN_ELSE:
+                if False:
+                    raise SyntaxError("Missing 'block' in penguinElse statement.")
+
             elif token_type == TokenType.RETURN_ICE:
                 if "value" not in token:
                     raise SyntaxError("Missing 'value' in returnIce statement.")
@@ -58,5 +62,9 @@ class Parser:
                 # Handle other token types or ignore
                 pass
 
+            # Recursively parse any nested blocks
+            if "block" in token:
+                block_tokens = token.get("block", [])
+                self.parse(block_tokens)
 
         return tokens  # In this simple parser, we return tokens as-is
